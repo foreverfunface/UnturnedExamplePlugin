ea047214968d5a3bfe4ca3c4880b60c7c28bcf4e
vse3211 <52658840.m@gmail.com>
2022-06-30 20:42:24 +0600

    * Buildable changes, empty base - Rename Class1.cs to Plugin.cs + Add links + Add main class + Add config class

---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using Rocket.Core.Plugins;
+
+namespace UnturnedExamplePlugin
+{
+    public class Plugin : RocketPlugin<PluginConfiguration>
+    {
+        protected override void Load()
+        {
+            // Enter code for load
+        }
+        protected override void Unload()
+        {
+            // Enter code for unload
+        }
+    }
+}
---
+++ PluginConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Rocket.API;
+
+namespace UnturnedExamplePlugin
+{
+    public class PluginConfiguration : IRocketPluginConfiguration
+    {
+        public void LoadDefaults()
+        {
+            // Enter config code
+        }
+    }
+}
