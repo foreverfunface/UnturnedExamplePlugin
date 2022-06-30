06d09e8e4426857aa8aab2b15fc8ce5ee5e5dd35
vse3211 <52658840.m@gmail.com>
2022-06-30 21:11:31 +0600

    * Buildable changes, command base + Add example command

---
+++ Commands/ExampleCommand.cs
@@ -0,0 +1,27 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using System;
+using System.Collections.Generic;
+
+namespace UnturnedExamplePlugin.Commands
+{
+    public class ExampleCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "example";
+
+        public string Help => "Use /example for get example message";
+
+        public string Syntax => "";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedChat.Say(caller, $"[{DateTime.Now}] {Plugin.Instance.Random.Next(10000)}"); // Say player message with Rocket
+        }
+    }
+}
